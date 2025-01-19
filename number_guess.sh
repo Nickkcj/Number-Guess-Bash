9fc3726cbde0aada1015864649be4e9e0ed25ded
Nickk Jasper <nicholas.jasper@grad.ufsc.br>
2025-01-19 22:00:00 +0000

    refactor: logic

--- number_guess.sh
+++ number_guess.sh
@@ -7,27 +7,24 @@ USER_LOGIN() {
   echo "Enter your username:"
   read USERNAME
 
-  # Check if username exists in the database
+  # Check if the username exists in the database
   USER_DATA=$($PSQL "SELECT user_id, games_played, best_game FROM users WHERE username='$USERNAME'")
 
-  # If no data found, it's a new user
   if [[ -z "$USER_DATA" ]]; then
+    # If no data is found, it's a new user
     echo "Welcome, $USERNAME! It looks like this is your first time here."
     # Insert new user into the database
     INSERT_USER=$($PSQL "INSERT INTO users (username) VALUES ('$USERNAME')")
+    # Retrieve the new user's ID
     USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
     GAMES_PLAYED=0
     BEST_GAME="None"
   else
-    # If data found, retrieve user stats
+    # If the user exists, retrieve their data
     USER_ID=$(echo "$USER_DATA" | cut -d '|' -f 1)
     GAMES_PLAYED=$(echo "$USER_DATA" | cut -d '|' -f 2)
     BEST_GAME=$(echo "$USER_DATA" | cut -d '|' -f 3)
-
-    # If BEST_GAME is NULL, set it to "None" for display
-    if [[ "$BEST_GAME" == "NULL" ]]; then
-      BEST_GAME="None"
-    fi
+    # Print the welcome back message
     echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
   fi
 }
@@ -54,16 +51,16 @@ PLAY_GAME() {
     elif [[ $GUESS -gt $SECRET_NUMBER ]]; then
       echo "It's lower than that, guess again:"
     else
-    # Update user stats
+      # Update user stats
       GAMES_PLAYED=$((GAMES_PLAYED + 1))
       $PSQL "UPDATE users SET games_played=$GAMES_PLAYED WHERE user_id=$USER_ID"
-      
-      if [[ $BEST_GAME == "None" || $NUMBER_OF_GUESSES -lt $BEST_GAME ]]; then
+
+      if [[ "$BEST_GAME" == "None" || $NUMBER_OF_GUESSES -lt $BEST_GAME ]]; then
         $PSQL "UPDATE users SET best_game=$NUMBER_OF_GUESSES WHERE user_id=$USER_ID"
       fi
+
       # Correct guess, print the success message and exit
       echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
-      # Exit cleanly
       exit 0
     fi
   done
@@ -72,6 +69,3 @@ PLAY_GAME() {
 # Main script logic
 USER_LOGIN
 PLAY_GAME
-
-
-
